4f05b821ddba941506d08382fc72b42ca6a8a283
germanny <germanny@gmail.com>
2022-12-08 09:06:27 -0600

    progress on day 6 (incomplete)

--- day-6/index.ts
+++ day-6/index.ts
@@ -6,21 +6,22 @@ import { checkDuplicateArrayItems } from "../utils";
 // Look for the first instance of all the letters in the string being different
 // Mark the index of the last letter in that string (remember that it will be 0-indexed!)
 
-const crawlString = (str: string): number => {
+const crawlString = (str: string, start: number): number => {
   let marker = 0;
   const splitStr = str.split("");
 
   splitStr.forEach((_, index) => {
     let charArray: string[] = [];
-    for (let i = index; i < index + 4; i++) {
+    for (let i = index; i < index + start; i++) {
       charArray.push(splitStr[i]);
     }
 
     const hasDuplicateLetters = charArray.some(checkDuplicateArrayItems);
-    if (!hasDuplicateLetters && marker === 0) marker = index + 4;
+    if (!hasDuplicateLetters && marker === 0) marker = index + start;
   });
   console.log({ marker });
   return marker;
 };
 
-crawlString(startOfPacketMarker);
+// const testMarker = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"; // first marker after character 29
+crawlString(startOfPacketMarker, 14);
